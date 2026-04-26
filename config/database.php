af21a33357b3cf7680990f46fb8c6d4608bd8283
Kush srivastava <kushsrivastava@Kushs-MacBook-Air.local>
2026-04-26 16:26:07 +0530

    added for .env..

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'host'     => '127.0.0.1',
-    'port'     => 3306,
-    'database' => 'compliance_saas',
-    'username' => 'root',
-    'password' => '',
+    'host'     => getenv('DB_HOST')     ?: '127.0.0.1',
+    'port'     => (int) (getenv('DB_PORT') ?: 3306),
+    'database' => getenv('DB_DATABASE') ?: 'compliance_saas',
+    'username' => getenv('DB_USERNAME') ?: 'root',
+    'password' => (($p = getenv('DB_PASSWORD')) !== false) ? $p : '',
     'charset'  => 'utf8mb4',
 ];
